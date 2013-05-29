bc18ad85863446f0de4890a94d481215d67c9d35
defm03 <yutsuro@gmail.com>
2013-05-29 20:01:58 +0200

    testing opengl mouse and music

--- src/display/opngl.py
+++ src/display/opngl.py
@@ -9,13 +9,19 @@ from pyglet.window import mouse
 
 """
 
+# class stuff here
+
+"""
+   Main varibles to display
+"""
 win = pygl.window.Window()
 label = pyglet.text.Label('DZA WARLDO',
                           font_name='Times New Roman',
                           font_size=36,
                           x=win.width//2,y=win.height//2,
                           anchor_x='center', anchor_y='center')
-image = pyglet.resource.image('test.png')
+image = pygl.resource.image('test.png')
+music = pygl.resource.media('test_battlelolis.mp3')
 
 # Input for window > key, mouse etc.
 @win.event
@@ -30,7 +36,13 @@ def on_key_press(symbol, modifiers):
     print '--LEFT_key_press'
   elif symbol == key.ENTER:
     print '--ENTER_key_press'
-
+@win.event
+def on_mouse_press(x,y,button,modifiers):
+  if button == mouse.LEFT:
+    print 'LMB_press'
+  if button == mouse.RIGHT:
+    print 'RMB_press'
+    win.push_handlers(pyglet.window.event.WindowEventLogger())
 # Printing to window
 @win.event
 def on_draw():
@@ -43,5 +55,8 @@ def on_draw():
   glVertex2i(100,150)
   glEnd()
   label.draw()
-
+  
+music.play()
 pyglet.app.run()
+
+
