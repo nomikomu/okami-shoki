1eb5c88f46487c84ca59c8d081432e9cb6b34db1
defm03 <yutsuro@gmail.com>
2013-05-29 19:11:24 +0200

    testing opengl vertex

--- src/display/opngl.py
+++ src/display/opngl.py
@@ -1,6 +1,14 @@
-import pyglet
-win = pyglet.window.Window()
+import pyglet as pygl
+from pyglet.gl import *
+
+win = pygl.window.Window()
 @win.event
 def on_draw():
-  win.clear()
+  glClear(GL_COLOR_BUFFER_BIT)
+  glBegin(GL_POINTS)
+  glVertex2i(50,50)
+  glVertex2i(75,100)
+  glVertex2i(100,150)
+  glEnd()
+
 pyglet.app.run()
