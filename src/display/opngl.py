86697d624228e159a2c12a17daab6a08bbcafd71
defm03 <yutsuro@gmail.com>
2013-05-29 20:37:12 +0200

    Working on screen config - getting attributes and create context

--- src/display/opngl.py
+++ src/display/opngl.py
@@ -4,15 +4,32 @@ from pyglet.window import key
 from pyglet.window import mouse
 """
   TODO:
-    
-
-
-"""
-
-# class stuff here
+    Config -->
+      __init__ - screnn setting
+      get_gl_atrributes - conditions
+      is_complete - conditions
 
 """
-   Main varibles to display
+
+class Config:
+  def __init__(self,scrnn_x,scrnn_y,buff_size):
+    on_resize(self.scrnn_x,self.scrnn_y)
+    buffer_size=self.buff_size
+  def get_gl_atrributes():
+    return self
+  def is_complete():
+    return self
+
+def get_platform():
+  platform = pyglet.window.get_platform()
+  display = platform.get_default_display()
+
+"""
+   Main varibles to use
+    win    ->  name for obj creating window
+    lablel ->  'DZA WARLDO' text; formating, placement
+    image  ->  reading image file
+    music  ->  reading music file
 """
 win = pygl.window.Window()
 label = pyglet.text.Label('DZA WARLDO',
@@ -36,6 +53,7 @@ def on_key_press(symbol, modifiers):
     print '--LEFT_key_press'
   elif symbol == key.ENTER:
     print '--ENTER_key_press'
+
 @win.event
 def on_mouse_press(x,y,button,modifiers):
   if button == mouse.LEFT:
@@ -43,10 +61,12 @@ def on_mouse_press(x,y,button,modifiers):
   if button == mouse.RIGHT:
     print 'RMB_press'
     win.push_handlers(pyglet.window.event.WindowEventLogger())
+
 # Printing to window
 @win.event
 def on_draw():
 
+  get_platform()
   glClear(GL_COLOR_BUFFER_BIT)
   image.blit(win.width//2-84,275)
   glBegin(GL_POINTS)
