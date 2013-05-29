e05d1f7555dc4f8b3749d86d95d0f7c3e7ca22c0
defm03 <yutsuro@gmail.com>
2013-05-29 19:15:35 +0200

    testing opengl text draw

--- src/display/opngl.py
+++ src/display/opngl.py
@@ -2,6 +2,12 @@ import pyglet as pygl
 from pyglet.gl import *
 
 win = pygl.window.Window()
+label = pyglet.text.Label('DZA WARLDO',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=win.width//2,y=win.height//2,
+                          anchor_x='center', anchor_y='center')
+
 @win.event
 def on_draw():
   glClear(GL_COLOR_BUFFER_BIT)
@@ -10,5 +16,5 @@ def on_draw():
   glVertex2i(75,100)
   glVertex2i(100,150)
   glEnd()
-
+  label.draw()
 pyglet.app.run()
