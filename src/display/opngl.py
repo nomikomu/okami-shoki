e71adeb2098af0c657dbe1ed418a7cc63ad35187
defm03 <yutsuro@gmail.com>
2013-05-29 19:24:46 +0200

    testing opengl image blit

--- src/display/opngl.py
+++ src/display/opngl.py
@@ -7,14 +7,17 @@ label = pyglet.text.Label('DZA WARLDO',
                           font_size=36,
                           x=win.width//2,y=win.height//2,
                           anchor_x='center', anchor_y='center')
-
+image = pyglet.resource.image('test.png')
 @win.event
 def on_draw():
+
   glClear(GL_COLOR_BUFFER_BIT)
+  image.blit(win.width//2-84,275)
   glBegin(GL_POINTS)
   glVertex2i(50,50)
   glVertex2i(75,100)
   glVertex2i(100,150)
   glEnd()
   label.draw()
+
 pyglet.app.run()
